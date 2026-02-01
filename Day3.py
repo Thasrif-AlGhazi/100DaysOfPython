e8d4fd27b0118411e04c98dd5ba3b187f84b24e0
Thasrif-AlGhazi <238999460+Thasrif-AlGhazi@users.noreply.github.com>
2026-02-01 12:49:22 +0530

    Day 3

--- Day3.py
+++ Day3.py
@@ -4,6 +4,6 @@ a = float(input("Editing Hours: "))
 b = float(input("Python Hours: "))
 c = int(input("Days Completed: "))
 
-Consistent = (a+b) > 5
+Consistent = (a+b) < 5
 
 print(f"I have Practiced for {a+b} hours. Consistency Status {Consistent}")
