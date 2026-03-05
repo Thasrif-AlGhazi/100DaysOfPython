a552a21558c1af33fbd81a6c376dab7b20f49091
Thasrif-AlGhazi <238999460+Thasrif-AlGhazi@users.noreply.github.com>
2026-03-05 21:46:31 +0530

    Login Basic with Return

---
+++ Day6.py
@@ -0,0 +1,16 @@
+def is_valid(username, password):
+   
+    if username == 'admin' or (username == 'user' and password == 'qweasd'):
+        return True
+    else:
+        return False
+
+userr = input("Enter username: ")
+pwd = input("Enter password: ")
+
+result = is_valid(userr, pwd)
+
+if result:
+    print("Login Successful")
+else:
+    print("Login Failead")
