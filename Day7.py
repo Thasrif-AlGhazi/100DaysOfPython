bd025e7e46af7ede55e1404f7b8b4005b1644e9b
Thasrif-AlGhazi <238999460+Thasrif-AlGhazi@users.noreply.github.com>
2026-03-07 00:37:32 +0530

    FizzBuzz Logic

---
+++ Day7.py
@@ -0,0 +1,12 @@
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    return str(n)
+
+limit = int(input("Enter limit: "))
+result = fizzbuzz(limit)
+print(result)
